b7d81573a279c09f4d57a131d8ffe107e080ca81
SUMIT KUMAR MONDAL <50587174+cymntx@users.noreply.github.com>
2026-02-24 15:56:00 +0530

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,11 +1,11 @@
-import pytest
 import sys
 import os
 import torch
+from model import MultiModalNet
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'))
 )
-from model import MultiModalNet
+
 
 
 def test_model_initialization():
