901097180f12c38a2efc8b305a5b270b08ec25f7
SUMIT KUMAR MONDAL <50587174+cymntx@users.noreply.github.com>
2026-02-24 18:31:39 +0530

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,10 @@
 import os
 import tempfile
 import pytest
+import sys
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src'))
+)
 import torch
 from PIL import Image
 import numpy as np
